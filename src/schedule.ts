30cbcefb5a5aed95bc1d0981b207addf64850276
Muhammet Ali AKBAY <akbay@plutus.it>
2021-06-26 16:32:49 +0300

    Added "sorting" field with hardcoded "asc" value in ScheduleOffsets type

--- src/schedule.ts
+++ src/schedule.ts
@@ -16,10 +16,14 @@ export class InvalidOffsetsError extends ScheduleParsingError {
     }
 }
 
+export type ScheduleOffsets = number[] & {sorting: "asc"}
+
 const scheduleRegex = /^[0-9]+s(-[0-9]+s)*$/
-export function parseSchedule(text: string): number[] & {sorting?: "asc"} {
+export function parseSchedule(text: string): ScheduleOffsets {
     if (text.length === 0) {
-        return []
+        const ret = [] as ScheduleOffsets;
+        ret.sorting = "asc"
+        return ret
     } else if (!scheduleRegex.test(text)) {
         throw new ScheduleSyntaxError("Invalid schedule syntax, regex: /" + scheduleRegex.source + "/g, schedule: " + JSON.stringify(text))
     }
@@ -29,13 +33,14 @@ export function parseSchedule(text: string): number[] & {sorting?: "asc"} {
         part => part.substring(0, part.length-1)
     ).map(
         numStr => Number(numStr)
-    )
+    ) as ScheduleOffsets
 
     for (let i = 1; i < offsets.length; i ++) {
         if (offsets[i] <= offsets[i - 1]) {
             throw new InvalidOffsetsError("Schedule's offsets must be sorted in ascending order with no duplication")
         }
     }
+    offsets.sorting = "asc"
 
     return offsets
 }
