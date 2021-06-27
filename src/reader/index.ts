51fce8b8296dc33cdcc3d02ff19ff7340015058f
Muhammet Ali AKBAY <akbay@plutus.it>
2021-06-27 16:34:02 +0300

    Added comments to describe the behaviour of Reader interface and StringReader instance

--- src/reader/index.ts
+++ src/reader/index.ts
@@ -1,8 +1,17 @@
+/* `End` is a unique symbol which points the end of the reader's source. */
 export const End: unique symbol = Symbol('End')
 export type End = typeof End;
 
 export interface Reader {
+    /**
+     * Reads next character and returns. If no character left to be
+     * red in the source, then it returns `End` symbol.
+     */
     readCharacter(): Promise<string | End>
 
+    /**
+     * Attempts to close related resources of the underlying source
+     * that the reader fetches characters from.
+     */
     close(): void
 }
--- src/reader/string.ts
+++ src/reader/string.ts
@@ -1,5 +1,11 @@
 import {End, Reader} from "./";
 
+/**
+ * Simplest implementation of the `Reader` interface. `StringReader`
+ * takes a string as the source of characters to be red. And, counts
+ * the offset using an accumulator field internally by incrementing it
+ * after each read operation.
+ */
 export class StringReader implements Reader {
     private offset = 0
     private length: number
@@ -10,9 +16,19 @@ export class StringReader implements Reader {
         this.length = source.length
     }
 
+    /**
+     * Does nothing since the `StringReader` doesn't utilize
+     * any resource. JS Engine should simply remove the object
+     * from the memory when it gets un-pointed.
+     */
     close() {
     }
 
+    /**
+     * Returns the next character in the string source, if there
+     * is no character left (when offset gets equals to the length)
+     * then returns `End` symbol to indicate the end of the source.
+     */
     async readCharacter() {
         if (this.length === this.offset) {
             return End
