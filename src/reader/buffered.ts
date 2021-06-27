5096d54e3f527050e0b3cfbdbb14557a83dcbade
Muhammet Ali AKBAY <akbay@plutus.it>
2021-06-27 16:56:23 +0300

    Added comments to describe the behaviour of stream and buffered reader instances

--- src/reader/buffered.ts
+++ src/reader/buffered.ts
@@ -10,6 +10,12 @@ export class UnableToPushEndError extends BufferedReaderError {
         super("Can not push `End` at this state");
     }
 }
+
+/**
+ * A `Reader` implementation which provides support for
+ * pushing back the character red recently, to make it
+ * available to be red again in subsequent read operations.
+ */
 export class BufferedReader implements Reader {
     private buffer: [...string[], ...[End?]] = []
     constructor(
@@ -17,10 +23,19 @@ export class BufferedReader implements Reader {
     ) {
     }
 
+    /**
+     * Calls the close method of the source reader.
+     */
     close() {
         this.source.close()
     }
 
+    /**
+     * Reads the next character from the buffer, if there is
+     * no character buffered (pushed back) then tries to fetch
+     * the next one from the source reader, if even it has no
+     * character to provide, then returns `End` symbol.
+     */
     async readCharacter(): Promise<string | End> {
         if (this.buffer.length > 0) {
             const char = this.buffer[0]
@@ -39,6 +54,12 @@ export class BufferedReader implements Reader {
         }
     }
 
+    /**
+     * Pushes given character back to the buffer to make it
+     * available to be red in the next first read operation.
+     * If `End` symbol is provided to be pushed back, then
+     * it checks whether the readable source is ended too.
+     */
     pushCharacter(char: string | End): void {
         if (char === End) {
             if (this.buffer[0] !== End) {
--- src/reader/stream.ts
+++ src/reader/stream.ts
@@ -19,6 +19,12 @@ export class ConcurrentReadError extends StreamReaderError {
     }
 }
 
+/**
+ * An implementation of `Reader` interface to provide support for
+ * reading any (utf-8 encoded) `Readable` instance character by character.
+ * `StreamReader` also holds a buffer internally to make next characters
+ * ready before the demand.
+ */
 export class StreamReader implements Reader {
     private endPromise: Promise<void>;
     private queue: { chunk: string, offset: number }[] = []
@@ -29,6 +35,8 @@ export class StreamReader implements Reader {
     constructor(
         private source: Readable
     ) {
+        /* `endPromise` is a promise which will be resolved/rejected when
+        * the source readable gets ended or throws an error */
         this.endPromise = new Promise<void>(
             (resolve, reject) => {
                 if (source.readableEnded) {
@@ -39,14 +47,21 @@ export class StreamReader implements Reader {
                 }
             }
         )
+        /* Putting the source into the `flowing` mode, and listening for
+        * the chunks of the data */
         this.source.on('data', chunk => {
             if (typeof chunk === 'string') {
+                /* Only string chunks are supported. */
                 if (chunk.length > 0) {
+                    /* Put the chunk in the queue, with an offset accumulator
+                    * which has 0 as the initial value */
                     this.queue.push({
                         chunk,
                         offset: 0
                     })
                     this.queuedTotal += chunk.length
+                    /* If there is a concurrent read ongoing currently, then
+                    * notify the callback that there is new characters to read. */
                     this.notifyCallback?.()
                     this.controlFlow()
                 }
@@ -58,20 +73,31 @@ export class StreamReader implements Reader {
 
     private controlFlow() {
         if (this.queuedTotal >= this.queueCapacity) {
+            /* If there are too much chunks in the queue, which waits to be red, then
+            * switch the source out of `flowing` mode ...  */
             this.source.pause()
         } else {
+            /* ... Otherwise, make sure that the source is in `flowing` mode. */
             this.source.resume()
         }
     }
 
+    /**
+     * Closes the underlying readable source.
+     * @param error Optionally, an error can be specified as the reason of closing the readable.
+     */
     close(error?: Error) {
         this.source.destroy(error)
     }
 
     private async readCharacterInternal(): Promise<string> {
         if (this.notifyCallback != undefined) {
+            /* If a `notifyCallback` is specified already, then there is another
+            * read operation runs concurrently. */
             throw new ConcurrentReadError()
         } else if (this.queue.length === 0) {
+            /* If no characters available in the queue (the buffer), then waits
+            * for being notified via the `notifyCallback` by the data listener. */
             await new Promise<void>((resolve, reject) => {
                 if (this.notifyCallback != undefined) {
                     reject(new ConcurrentReadError());
@@ -85,18 +111,30 @@ export class StreamReader implements Reader {
             this.notifyCallback = undefined
         }
 
+        /* Pull the first chunks in the queue */
         const entry = this.queue[0]
         let char: string;
+        /* Increase the offset accumulator for the chunk which is just pulled. */
         const offset = entry.offset++;
+        /* Take the character in the chunk at the position which is specified by
+        * the value of the offset accumulator just before being incresed. */
         char = entry.chunk[offset]
         this.queuedTotal--;
         if (entry.offset === entry.chunk.length) {
+            /* If the offset accumulator reaches the end of the chunk, then
+            * pop the chunk from the queue to make next one available for subsequent
+            * read operations. */
             this.queue.splice(0, 1)
         }
         this.controlFlow()
         return char
     }
 
+    /**
+     * Reads the next character from the buffer which is being filled
+     * of the data retrieved from the readable source. If there is no
+     * character left to be red, returns `End` symbol.
+     */
     readCharacter(): Promise<string | End> {
         return Promise.race([
             this.endPromise.then(() => End) as Promise<End>,
