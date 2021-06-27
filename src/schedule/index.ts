8b422f62d44dc3cfc88f1a08cdc1cf91667ccc64
Muhammet Ali AKBAY <akbay@plutus.it>
2021-06-27 17:33:49 +0300

    Added comments to describe the behaviour of schedule and schedule list instances

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -25,6 +25,14 @@ export class InvalidOffsetsError extends ScheduleParsingError {
 export type ScheduleOffsets = number[] & {sorting: "asc"}
 
 const scheduleRegex = /^[0-9]+s(-[0-9]+s)*$/
+
+/**
+ * Takes a string parameter and tries to parse it as a value
+ * as present in the "schedule" column in the given CSV document.
+ * Parses the offset numbers, checks whether they are in a
+ * sorted order (ascending) and distinct.
+ * @param text The string value to be parsed as the "schedule" column
+ */
 export function parseSchedule(text: string): ScheduleOffsets {
     if (text.length === 0) {
         const ret = [] as ScheduleOffsets;
@@ -34,14 +42,19 @@ export function parseSchedule(text: string): ScheduleOffsets {
         throw new ScheduleSyntaxError("Invalid schedule syntax, regex: /" + scheduleRegex.source + "/g, schedule: " + JSON.stringify(text))
     }
 
+    /* Each part of offsets is separated by a dash (-) character */
     const parts = text.split('-')
     const offsets = parts.map(
+        /* Remove "s" suffix from the part */
         part => part.substring(0, part.length-1)
     ).map(
+        /* Parse the part as a number */
         numStr => Number(numStr)
     ) as ScheduleOffsets
 
     for (let i = 1; i < offsets.length; i ++) {
+        /* Every number in the `offsets` list must be greater than the
+        * previous one. */
         if (offsets[i] <= offsets[i - 1]) {
             throw new InvalidOffsetsError("Schedule's offsets must be sorted in ascending order with no duplication")
         }
--- src/schedule/list.ts
+++ src/schedule/list.ts
@@ -1,5 +1,10 @@
 import {Schedule} from "./";
 
+/**
+ * A wrapper implementation for a list to store `Schedule` objects
+ * as sorted in an ascending order. Also, accumulates the total number of
+ * all reminder offsets.
+ */
 export class ScheduleList {
     private list: Schedule[] = []
     private total: number = 0
@@ -10,13 +15,23 @@ export class ScheduleList {
 
     private locate(offset: number): number {
         for (let i = 0; i < this.list.length; i ++) {
+            /* Scan each entries in the current list against their offsets,
+            * by comparing the given one to find the correct position. */
             if (this.list[i].offsets[0] > offset) {
                 return i
             }
         }
+        /* If it should be placed at the end of the list, then return the
+        * length of the list as the desired position */
         return this.list.length
     }
 
+    /**
+     * Find correct positions (to keep the list well sorted) for each
+     * given schedule entries, and place them in the list. The entries
+     * with empty reminder offset list will be ignored.
+     * @param schedules The schedule entries to be placed in the list
+     */
     put(...schedules: Schedule[]) {
         for (const schedule of schedules) {
             if (schedule.offsets.length > 0) {
@@ -27,20 +42,35 @@ export class ScheduleList {
         }
     }
 
+    /**
+     * Return the total number of reminder offsets present in the list.
+     */
     getTotal() {
         return this.total
     }
 
+    /**
+     * Removes the first schedule entry in the list (the one has the least value
+     * of reminder offset), returns the object.
+     */
     pop(): Schedule {
         const schedule = this.list.splice(0, 1) [0]
         this.total -= schedule.offsets.length
         return schedule
     }
 
+    /**
+     * Returns the first schedule entry in the list (the one has the least value
+     * of reminder offset).
+     */
     peek(): Schedule {
         return this.list[0]
     }
 
+    /**
+     * Returns the least value of the reminder offset present in the list. If no
+     * schedule object is placed, then returns `null`
+     */
     nextOffset(): number | null {
         return this.list[0]?.offsets?.[0] ?? null
     }
